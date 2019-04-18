069adea374d712316dcf13611fb312863c42e69e
Ashwin Ramachandran <ashwin.ramachandran@thundertoken.com>
2019-04-18 13:15:40 -0700

    Remove external modifier to allow for embedded functions, fix library signature

--- RandomLibrary.sol
+++ RandomLibrary.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.25;
 
 library GenerateRandomNumber {
-    function getRandom() external returns (bytes32) {
+    function generateRandom() internal returns (uint256) {
         address addr = address(0x8cC9C2e145d3AA946502964B1B69CE3cD066A9C7);
         bytes4 randomSignature = bytes4(keccak256("generateRandom()"));
-        bytes32 value;
+        uint256 value;
 
         assembly {
             let o := mload(0x40)
